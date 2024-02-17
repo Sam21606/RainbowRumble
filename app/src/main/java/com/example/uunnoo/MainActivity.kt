6ce626567ac3413d2cb603f1649edf0fd9f27d3a
sam <samu.schuetz21@gmail.com>
2024-02-17 11:15:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/uunnoo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.uunnoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    fun init() {
+        Datastore.createCards()
+
+    }
+}
